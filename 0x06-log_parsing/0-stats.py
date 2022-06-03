ed7b59af56dae1100db8f549821005d996449abd
Andres Blandon <andblandon@hotmail.com>
2022-06-03 04:03:11 +0000

    Update - error

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     listCodes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                  '404': 0, '405': 0, '500': 0}
     try:
-        for data in sys.stdin:
+        for data in stdin:
             # check empty
             if data == "":
                 continue

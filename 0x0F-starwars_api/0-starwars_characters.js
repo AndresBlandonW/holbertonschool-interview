8610fa0fe3267317c518a65dc26a6600f173ce75
Andres Blandon <andblandon@hotmail.com>
2022-08-12 04:00:15 +0000

    starwars api nodejs

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,25 +1,24 @@
 #!/usr/bin/node
+const request = require('request')
 
-const request = require('request');
-var url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2]
 
 request.get(url, async (error, response, body) => {
-  if (error) { return; }
+  if (error) { return }
 
-  const data = JSON.parse(body);
+  const data = JSON.parse(body)
 
   const getNameChar = urls => {
     const promise = new Promise((resolve, reject) => {
       request.get(urls, (error, response, body) => {
-        if (error) { reject(error); } else { resolve(body); }
-      });
-    });
-    return promise;
-  };
+        if (error) { reject(error) } else { resolve(body) }
+      })
+    })
+    return promise
+  }
 
   for (let i = 0; i < (data.characters).length; i++) {
-    const result = await getNameChar(data.characters[i]);
-    console.log(JSON.parse(result).name);
+    const result = await getNameChar(data.characters[i])
+    console.log(JSON.parse(result).name)
   }
-});
+})

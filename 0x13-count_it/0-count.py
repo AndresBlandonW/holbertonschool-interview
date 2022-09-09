b52bba8997655248ebdcc48eed93e4fd3960a7c3
Andres Blandon <andblandon@hotmail.com>
2022-09-09 03:52:48 +0000

    update function

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,30 +1,34 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """ queries the Reddit API, parses the title of all hot articles,
     and prints a sorted count of given keywords
 """
+
 from requests import get
 
 
 def count_words(subreddit, word_list=[], after=''):
-	""" Calling function """
-	headers = {'user-agent': 'Mozilla/5.0\
+    """ Calling function """
+
+    headers = \
+        {'user-agent': 'Mozilla/5.0\
                (Macintosh; Intel Mac OS X 10_15_6)\
                AppleWebKit/537.36 (KHTML, like Gecko)\
                Chrome/85.0.4183.102 Safari/537.37'}
 
-	hot = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                 after)
-	hot_req = get(hot, headers=headers)
-	stuff = hot_req.json()
-	if stuff.get("data") and stuff['data'].get('children'):
-		children = stuff.get("data").get("children")
-		for child in children:
-			word_list.append(child.get("data").get("title"))
+    hot = \
+        'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit,
+                                                               after)
+    hot_req = get(hot, headers=headers)
+    stuff = hot_req.json()
+    if stuff.get('data') and stuff['data'].get('children'):
+        children = stuff.get('data').get('children')
+        for child in children:
+            word_list.append(child.get('data').get('title'))
 
-		page = stuff.get("data").get("after")
-		if page:
-			return count_words(subreddit, word_list, page)
+        page = stuff.get('data').get('after')
+        if page:
+            return count_words(subreddit, word_list, page)
 
-	if word_list == []:
-		return None
-	return word_list
+    if word_list == []:
+        return None
+    return word_list

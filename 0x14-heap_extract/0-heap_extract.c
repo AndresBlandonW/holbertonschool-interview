0fd6e3bca76494207fefba05f76fe06833445240
Andres Blandon <andblandon@hotmail.com>
2022-09-16 03:23:32 +0000

    heap extract

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extracts the root node of a Max Binary Heap
+ * @root: double pointer to the root node of the heapi
+ *
+ * Return: value stored in the root node, 0 on failure
+ **/
+
+int heap_extract(heap_t **root)
+{
+	if (!(*root) || !root)
+		return (0);
+
+	return ((*root)->n);
+}
